4e1fb4c098c222567c7aa8cafc29735b7667927a
Bernard Espanola <espanola.bernard@gmail.com>
2017-07-01 00:23:45 -0700

    finished module 1

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,10 +5,14 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>        
+        <title>Laravel</title>     
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">   <link rel="stylesheet" href="{{ URL::to('css/main.css')}}" />
     </head>
     <body>
-        @yield('content')
+        @include('partials.header')
+        <div class="container">
+            @yield('content')
+        </div>
     </body>
 </html>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,22 @@
 @extends('layouts.master')
 
 @section('content')
-<h1>Some Content</h1>
+    <div style="margin: 60px 0 0 0">
+        <h1>Some Content</h1>
+        <p>{{ 2 == 2 ? "Hello" : "Does not equal" }}</p>
+        @if(true)
+            <h2>TRUE</h2>
+        @else
+            <h2>FALSE</h2>
+        @endif
+        <hr />
+        @for($i = 0; $i < 5; $i++)
+            <p>{{$i + 1}} iteration</p>
+        @endfor
+        <hr />
+        <p>Non rendered script Code {{ "<script>alert('hello')</script>"}}</p>
+        <p>Render HTML Code {!! "<script>alert('hello')</script>"!!}</p>
+        <p class="testing">Checking URL Facade for main.css</p>        
+    </div>
 @endsection
 
